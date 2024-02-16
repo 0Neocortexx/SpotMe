2ecf1625998b8f8d2db2deba16d81f5aed3c1d5d
Jonathan Amorim <94270784+0Neocortexx@users.noreply.github.com>
2024-02-16 13:20:35 -0300

    First Commit

---
+++ src/main/java/com/spotme/project_spotme/ProjectSpotmeApplication.java
@@ -0,0 +1,13 @@
+package com.spotme.project_spotme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpotmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpotmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotme/project_spotme/ProjectSpotmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotme.project_spotme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpotmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
